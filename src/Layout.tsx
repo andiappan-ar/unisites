9bd18190602ef322c010553a0cdf0fc9d2751afb
haneendaoud <haneen.daoud@omnia.ae>
2025-01-30 01:42:50 +0300

    Multisite Components

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,26 +4,27 @@ import { Placeholder, LayoutServiceData, Field, HTMLLink } from '@sitecore-jss/s
 import config from 'temp/config';
 import Navigation from 'src/Navigation';
 import Scripts from 'src/Scripts';
-
+ 
 // Prefix public assets with a public URL to enable compatibility with Sitecore editors.
 // If you're not supporting Sitecore editors, you can remove this.
 const publicUrl = config.publicUrl;
-
+ 
 interface LayoutProps {
   layoutData: LayoutServiceData;
   headLinks: HTMLLink[];
 }
-
+ 
 interface RouteFields {
   [key: string]: unknown;
   Headline: Field;
 }
-
+ 
 const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
-  const { route } = layoutData.sitecore;
-
+  const { route,context }:any = layoutData.sitecore;
+ 
   const fields = route?.fields as RouteFields;
-
+  const BGColor : any  =context?.multisiteSettings?.BgColor?.value
+ 
   return (
     <>
       <Scripts />
@@ -34,12 +35,13 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
           <link rel={headLink.rel} key={headLink.href} href={headLink.href} />
         ))}
       </Head>
-
+ 
       <Navigation />
       {/* root placeholder for the app, which we add components to using route data */}
-      <div className="container">{route && <Placeholder name="jss-main" rendering={route} />}</div>
+      <div className="container" style={{ backgroundColor: BGColor }}>        
+        {route && <Placeholder name="headless-main" rendering={route} />}</div>
     </>
   );
 };
-
+ 
 export default Layout;
---
+++ src/components/GraphQLComponent.tsx
@@ -0,0 +1,38 @@
+import { Text, RichText, withDatasourceCheck, TextField, RichTextField } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+import styles from './Banner/HeroBanner.module.css'; // Optional: for styling
+
+type GraphQLComponentProps = ComponentProps & {
+  fields:{
+    data:{
+      results:{
+        name:TextField;
+        title: TextField;
+        description: RichTextField;
+      }
+    }
+  }
+  // fields: {    
+  //   Title: Field<string>;
+  // };
+};
+
+/**
+ * A simple Content Block component, with a heading and rich text block.
+ * This is the most basic building block of a content site, and the most basic
+ * JSS component that's useful.
+ */
+const GraphQLComponent = ({ fields }: GraphQLComponentProps): JSX.Element => (
+  <div 
+  className={styles.heroBanner}>
+     <div className={styles.content}>
+    <Text tag="h2" className="contentTitle" field={fields.data.results.title} />
+    <br/>
+    <RichText className="contentDescription" field={fields.data.results.description} />    
+    </div> 
+    </div>
+
+  
+);
+
+export default withDatasourceCheck()<GraphQLComponentProps>(GraphQLComponent);
