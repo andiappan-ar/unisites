bb004e2f07d8f5d3d7c88d1561c998bbd58bb2ac
AndiappanAR <andiappan.ravi@omnia.ae>
2025-01-30 10:34:49 +0400

    Build issue reverts in layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,27 +4,26 @@ import { Placeholder, LayoutServiceData, Field, HTMLLink } from '@sitecore-jss/s
 import config from 'temp/config';
 import Navigation from 'src/Navigation';
 import Scripts from 'src/Scripts';
- 
+
 // Prefix public assets with a public URL to enable compatibility with Sitecore editors.
 // If you're not supporting Sitecore editors, you can remove this.
 const publicUrl = config.publicUrl;
- 
+
 interface LayoutProps {
   layoutData: LayoutServiceData;
   headLinks: HTMLLink[];
 }
- 
+
 interface RouteFields {
   [key: string]: unknown;
   Headline: Field;
 }
- 
+
 const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
-  const { route }:any = layoutData.sitecore;
- 
+  const { route } = layoutData.sitecore;
+
   const fields = route?.fields as RouteFields;
-  // const BGColor : any  =context?.multisiteSettings?.BgColor?.value
- 
+
   return (
     <>
       <Scripts />
@@ -35,14 +34,12 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
           <link rel={headLink.rel} key={headLink.href} href={headLink.href} />
         ))}
       </Head>
- 
+
       <Navigation />
       {/* root placeholder for the app, which we add components to using route data */}
-          <div className="container" >     
-
-        {route && <Placeholder name="headless-main" rendering={route} />}</div>
+      <div className="container">{route && <Placeholder name="jss-main" rendering={route} />}</div>
     </>
   );
 };
- 
+
 export default Layout;
